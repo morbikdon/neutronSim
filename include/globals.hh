367efb1e1d720f47de70d56ff6135e07a9a166be
Berare <berare.gokturk@cern.ch>
2025-04-18 11:46:27 +0300

    globals are updated

--- include/globals.hh
+++ include/globals.hh
@@ -1,11 +1,8 @@
-// globals.hh
 #ifndef GLOBALS_HH
 #define GLOBALS_HH
 
 #include <atomic>
 
-#warning "globals.hh is being compiled"
-
-inline std::atomic<int> neutronCount(0);
+extern std::atomic<int> neutronCount;
 
 #endif
